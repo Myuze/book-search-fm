070a7c1ce3f134afeb43a8db633fbfe5ad448fc2
Myuze <Myuze@users.noreply.github.com>
2021-11-18 00:51:25 -0800

    Added GraphQL resolvers and typeDefs files

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,63 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('savedBooks')
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if(!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { authors, input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.({
+          authors: authors,
+          input
+        })
+      }
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const book = await Book.findOneAndDelete({
+          _id: bookId,
+        });
+
+        const user = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: [bookSchema]}}
+        )
+
+        return user;
+      }
+    }
+  }
+};
+
+module.exports = resolvers;
