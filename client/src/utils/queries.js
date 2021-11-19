e846dc80a53234a4a702c98ed9aa7571a89a698b
Myuze <Myuze@users.noreply.github.com>
2021-11-18 23:12:55 -0800

    Modified Client App.js for ApolloServer, and added queries and mutations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,14 @@
 import { gql } from '@apollo/client';
 
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      password
+      bookCount
+      savedBooks
+    }
+  }
+`;
